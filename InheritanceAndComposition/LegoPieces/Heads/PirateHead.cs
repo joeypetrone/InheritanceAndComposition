11f79cc13b92567b7f2ca887f1eda8d898f6fb65
Joey Petrone <joey.petrone@gmail.com>
2020-08-10 18:58:13 -0500

    Added Heads folder and PirateHead class

---
+++ InheritanceAndComposition/LegoPieces/Heads/PirateHead.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceAndComposition.LegoPieces.Heads
+{
+    class PirateHead
+    {
+        public bool HasPatch { get; set; }
+        public string FacialHair { get; set; }
+        public HairColor HairColor { get; set; }
+        public EyeColor EyeColor { get; set; }
+
+        public void Sing()
+        {
+            Console.WriteLine($"The {HairColor} pirate sang 'Yo ho ho and a bottle of rum' out of tune.");
+        }
+
+        public void Laugh()
+        {
+            Console.WriteLine("Har Har Har.");
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine("AAAAAAAAAARRRRRRGGGGGGGGGGGHHHHH");
+        }
+    }
+}
