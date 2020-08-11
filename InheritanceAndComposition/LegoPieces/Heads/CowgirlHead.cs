20dcd1f392610413b5fa7cf76a72330510130637
Joey Petrone <joey.petrone@gmail.com>
2020-08-10 19:22:36 -0500

    CowgirlHead class was created

---
+++ InheritanceAndComposition/LegoPieces/Heads/CowgirlHead.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceAndComposition.LegoPieces.Heads
+{
+    class CowgirlHead
+    {
+        //auto properties
+        public EyeColor EyeColor { get; set; }
+        public HairColor HairColor { get; set; }
+        public bool HasHat { get; set; }
+        public LipstickColor LipstickColor { get; set; }
+
+        public void HairFlip()
+        {
+            Console.WriteLine($"The cowgirl flips her {HairColor} hair. She doesn't even care.");
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine("Howdy!");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("The cowgirl eats her beans from a tin can.");
+        }
+
+    }
+}
