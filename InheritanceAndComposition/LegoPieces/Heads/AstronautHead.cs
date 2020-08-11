9ecfd6e026c869acc58be575ddc33247bdf42ffc
Joey Petrone <joey.petrone@gmail.com>
2020-08-10 19:29:21 -0500

    AstronautHead class created

---
+++ InheritanceAndComposition/LegoPieces/Heads/AstronautHead.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceAndComposition.LegoPieces.Heads
+{
+    class AstronautHead
+    {
+        //auto properties
+        public bool HasHelmet { get; set; } = true;
+        public HairColor HairColor { get; set; }
+        public EyeColor EyeColor { get; set; }
+        public bool HasFreckles { get; set; }
+
+        public void Greet()
+        {
+            Console.WriteLine("Major Tom to Ground Control....");
+        }
+
+        public void Observe()
+        {
+            Console.WriteLine("The Astronaut sees everything all at once. Across time.");
+        }
+    }
+}
